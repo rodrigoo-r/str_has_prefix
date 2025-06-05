c875a4d695bff74f894440027ea4d2ec33b80a09
rodrigoo-r <2nw53j32643y@gmail.com>
2025-06-05 08:12:17 -0500

    Add license information and copyright notice to library.h

--- library.h
+++ library.h
@@ -1,3 +1,14 @@
+/*
+ * This code is distributed under the terms of the GNU General Public License.
+ * For more information, please refer to the LICENSE file in the root directory.
+ * -------------------------------------------------
+ * Copyright (C) 2025 Rodrigo R.
+ * This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions; type `show c' for details.
+*/
+
+
 #ifndef STR_STARTS_WITH_LIBRARY_H
 #define STR_STARTS_WITH_LIBRARY_H
 
